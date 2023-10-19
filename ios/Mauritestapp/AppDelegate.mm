b6d0b6010e571a6e9e5c29eea63b76fa9d7f8716
mauriciomamrut <maurimamrut@gmail.com>
2023-10-19 14:43:54 -0300

    rename

--- ios/Mauritestapp/AppDelegate.mm
+++ ios/Mauritestapp/AppDelegate.mm
@@ -4,7 +4,7 @@
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"EffectusBase";
+  self.moduleName = @"Mauri test app";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
--- ios/MauritestappTests/MauritestappTests.m
+++ ios/MauritestappTests/MauritestappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface EffectusBaseTests : XCTestCase
+@interface MauritestappTests : XCTestCase
 
 @end
 
-@implementation EffectusBaseTests
+@implementation MauritestappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test{
   if (test(view)) {
